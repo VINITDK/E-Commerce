62f48be7bda51655e80b8d1a1023355b5b87e8fe
Vinit Divakar <108054187+VINITDK@users.noreply.github.com>
2023-08-20 23:14:03 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -3,18 +3,18 @@ const router = express.Router();
 
 // initializing products controller
 
-const productsController = require("../controller/product_controller");
+const productsController = require("../controllers/product_controller");
 
 // to get all the products
-router.get("/", productsController.products);
+router.get("/", productsControllers.products);
 
 // to create a product
-router.post("/create", productsController.create);
+router.post("/create", productsControllers.create);
 
 // to delete a product using it's ID
-router.delete("/:productID", productsController.delete);
+router.delete("/:productID", productsControllers.delete);
 
 // to update the quantity of a product
-router.post("/:productID/update_quantity/", productsController.updateQunatity);
+router.post("/:productID/update_quantity/", productsControllers.updateQunatity);
 
 module.exports = router;
